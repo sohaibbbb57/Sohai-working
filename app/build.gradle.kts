77967d3568f75410b384b138e83b5439fa6f6963
sohaibbbb57 <devranyuksekkaya8@gmail.com>
2026-09-02 14:28:15 +0300

    fix: downgrade core-ktx to 1.13.1 and set compileSdk to 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
 
     implementation("androidx.activity:activity-compose:1.13.0")
     implementation("androidx.activity:activity-ktx:1.13.0")
-    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.core:core-ktx:1.13.1"
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.10.0")
     implementation("androidx.compose.material3:material3")
